c19146657f834726eb50085b493ceab8fd740e97
moukthik <narayanam.moukthik@gmail.com>
2020-05-11 06:06:12 +0530

    contact us popup width fix

--- src/components/contact-us/ContactUs.jsx
+++ src/components/contact-us/ContactUs.jsx
@@ -1,13 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState, useLayoutEffect } from 'react';
 import { Modal, Container } from 'react-bootstrap';
 import './ContactUs.css';
 import { useHistory } from 'react-router-dom';
 
 export default function ContactUs() {
+    const [popupWidth, setPopupWidth] = useState(0);
     const history = useHistory();
     function handleFormClose() {
         history.push("/home");
     }
+
+    useEffect(() => {
+        function handleResize() {
+            const confirmPopoup = document.getElementById("contactUsPopup");
+            const width = confirmPopoup ? confirmPopoup.scrollWidth : "0";
+            setPopupWidth(width);
+        }
+
+        window.addEventListener('resize', handleResize)
+
+        return _ => {
+            window.removeEventListener('resize', handleResize)
+        }
+
+    }, []);
+
+    useLayoutEffect(() => {
+        const confirmPopoup = document.getElementById("contactUsPopup");
+        const width = confirmPopoup ? confirmPopoup.scrollWidth : "0";
+        setPopupWidth(width)
+    }, [])
+
     return (
         <>
             <Container fluid className="contact-us-page-container"></Container>
@@ -17,6 +40,7 @@ export default function ContactUs() {
                 aria-labelledby="contained-modal-title-vcenter"
                 onHide={handleFormClose}
                 scrollable
+                id="contactUsPopup"
             >
                 <Modal.Header closeButton>
                     <Modal.Title id="contained-modal-title-vcenter">
@@ -27,7 +51,7 @@ export default function ContactUs() {
                     <div className="contact-us-form-container">
                         <iframe
                             src="https://docs.google.com/forms/d/e/1FAIpQLSchu0Wy7daqnIKjXXg0cEuVnD5OcMJ8tL_tYzxO_Y38Ri8pTg/viewform?embedded=true"
-                            width="640"
+                            width={popupWidth}
                             height="1240"
                             frameBorder="0"
                             marginHeight="0"
